258c10ca655f2fc71209bff89f40c05af855477f
emma-data-web <emmanuelnwankwo690@gmail.com>
2025-07-29 08:01:51 +0100

    pulled the data from the database and updated the env file

---
+++ db_pull.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+import pandas as pd
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+# use a function to call and hold the dataset, you can call yours directly!
+def get_data():
+  db_url = f"mysql+pymysql://{os.getenv('db_username')}:{os.getenv('db_password')}@{os.getenv('db_host')}:{os.getenv('db_port')}/{os.getenv('db_database')}"
+  engine = create_engine(db_url)
+  query = 'select * from titanic_train'
+  df = pd.read_sql(query, engine)
+  return df
+
+
+print(get_data().head())
---
+++ pipeline.py
@@ -0,0 +1,5 @@
+from db_pull import get_data
+
+df = get_data().head(10)
+
+print(df)
